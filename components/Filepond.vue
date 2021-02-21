3cd0f7d2201b212aeb92dd915b8e864be1fe6278
Peter Boomsma <peter@cleanpixel.nl>
2021-02-21 14:32:08 +0100

    users now have to add a file name that doesn't already exist

--- components/Filepond.vue
+++ components/Filepond.vue
@@ -2,13 +2,22 @@
   <div>
     <file-pond
       ref="pond"
-      label-idle="Drop files here..."
+      label-idle="Drop file here..."
       v-bind:allow-multiple="false"
       accepted-file-types="image/jpeg, image/png"
-      :files="myFiles"
-      @addfile="toggleButton(); setFile()"
+      @addfile="setFile"
       @removefile="toggleButton()"
     />
+    <label>
+      Name has to be more than 3 characters long.
+      <input
+        type="text"
+        placeholder="name of the game"
+        v-on:keyup='toggleButton($event.target.value)'
+        v-model="gameName"
+        :disabled="file ? false : true"
+      />
+    </label>
     <button
       @click="upload()"
       class="button--grey"
@@ -27,6 +36,9 @@ import "filepond/dist/filepond.min.css";
 import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+import { debounce } from 'lodash';
+import { mapGetters } from 'vuex';
+import listImages from '~/mixins/listImages.js';
 
 AWS.config.update({
   credentials: new AWS.CognitoIdentityCredentials({
@@ -37,31 +49,55 @@ AWS.config.update({
 
 const FilePond = vueFilePond(
   FilePondPluginFileValidateType,
-  FilePondPluginImagePreview
+  FilePondPluginImagePreview,
 );
 
 export default {
+  mixins: [listImages ],
   data() {
     return {
-      myFiles: [],
+      gameName: '',
+      fileExtension: '',
       images: [],
       uploadDisabled: true,
       file: null,
+      gallery: [],
     }
   },
   methods: {
-    setFile () {
+    setFile (error, file) {
+      this.fileExtension = '.' +  file.fileExtension
       this.file = this.$refs.pond.getFile().file
+      this.toggleButton()
     },
-    toggleButton () {
-      this.uploadDisabled = this.$refs.pond.getFiles().length > 0 ? false : true
+
+    toggleButton: debounce(function (gameName) {
+      if (this.checkIfGameIsDuplicate(this.gallery, gameName)) {
+        console.log('game already exists')
+        this.uploadDisabled = true
+      } else {
+        if (gameName && gameName.length > 3) {
+          this.uploadDisabled = this.$refs.pond.getFiles().length > 0 ? false : true
+        } else {
+          this.uploadDisabled = true
+        }
+      }
+
+    }, 100),
+
+    checkIfGameIsDuplicate (games, game) {
+      for (const item of games) {
+        if (item.Key.substr(0, item.Key.lastIndexOf(".")) === game) return true
+      }
+      return false
     },
+
     upload () {
       const upload = new AWS.S3.ManagedUpload({
         params: {
           Bucket: 'gamesnap',
-          Key: this.file.name,
-          Body: this.$refs.pond.getFile().file
+          Key: this.gameName + this.fileExtension,
+          Body: this.file
         }
       })
 
@@ -81,21 +117,48 @@ export default {
     addImageToGallery () {
       localforage.getItem('myGallery').then((value) => {
           const myArray = value || []
-          myArray.push({ Key: this.file.name })
+          myArray.push({ Key: this.gameName + this.fileExtension })
           localforage.setItem('myGallery', myArray)
       }).catch(function(err) {
           console.log(err);
       });
     }
-  }
+  },
+
+  computed: {
+    ...mapGetters({
+      GalleryStore: 'gallery/getGallery',
+    })
+  },
+
+  watch: {
+    GalleryStore: {
+      handler (GalleryStore) {
+        this.gallery = GalleryStore
+      }
+    }
+  },
+
+  mounted () {
+    console.log(this.GalleryStore.length === 0)
+    this.GalleryStore.length === 0 ? this.listImages() : this.gallery = this.GalleryStore;
+  },
 
 
 }
 </script>
 
 <style lang="scss">
+input[type="text"]:disabled {
+  background: red;
+}
 button:disabled,
 button[disabled] {
   color: red
+}
+input {
+  background: grey;
+  color: white;
+  padding: 12px;
 }
 </style>
